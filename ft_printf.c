8283734303d712d508944c6ee564f16ce5429e09
DisDylan <poinsu.dylan@gmail.com>
2020-12-08 15:00:12 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,63 +12,6 @@
 
 #include "ft_printf.h"
 
-static int ft_getdigits(const char *format)
-{
-	int i;
-	int res;
-
-	i = 0;
-	res = 0;
-	while (ft_isdigit(format[i]))
-	{
-		i *= 10;
-		i += format[i] + 48;
-	}
-	return (i);
-}
-
-static void	ft_star(const char *str, ft_flags flags, int pos, va_list arg)
-{
-	if (str[pos - 1] == '0')
-		flags.dot = va_arg(str, int);
-	else if (str[pos - 1] == '%')
-		flags.width = va_arg(str, int);
-	else if (str[pos - 1] == '.')
-		flags.width = va_arg(str, int);
-}
-
-static void	get_flags(va_list args, const char *format, ft_flags flags, char *conversion)
-{
-	int i;
-
-	i = 0;
-	while (!(ft_chrchr(conversion, format[i])) && format[i])
-	{
-		if (format[i] == '-')
-		{
-			i++;
-			flags.minus = 1;
-		}
-		else if (format[i] == '0' && format[i - 1] != '-')
-		{
-			i++;
-			flags.zero = ft_getdigit(format + i);
-		}
-		else if (format[i] == '.')
-		{
-			i++;
-			flags.dot = ft_getdigit(format + i);
-		}
-		else if (format[i] == '*')
-			ft_star(format, flags, i, args);
-		else if (isdigit)
-			flags.width = ft_getdigits(format + i);
-		else
-			i++;
-		
-	}
-}
-
 static void put_item(char c, va_list arg)
 {
 	if (c == 'c')
