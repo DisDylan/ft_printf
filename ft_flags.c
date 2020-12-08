87e826061a37389181fef186d4fd920dd5ba3ae6
DisDylan <poinsu.dylan@gmail.com>
2020-12-08 14:56:05 +0100

    swap some functions to an other file

--- ft_flags.c
+++ ft_flags.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_flags.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -37,7 +37,7 @@ static void	ft_star(const char *str, ft_flags flags, int pos, va_list arg)
 		flags.width = va_arg(str, int);
 }
 
-static void	get_flags(va_list args, const char *format, ft_flags flags, char *conversion)
+void	get_flags(va_list args, const char *format, ft_flags flags, char *conversion)
 {
 	int i;
 
@@ -67,63 +67,4 @@ static void	get_flags(va_list args, const char *format, ft_flags flags, char *co
 			i++;
 		
 	}
-}
-
-static void put_item(char c, va_list arg)
-{
-	if (c == 'c')
-		ft_putchar(va_arg(arg, char));
-	else if (c == 's')
-		ft_putstr(va_arg(arg, char*));
-	else if (c == 'p')
-		write(1, &arg, 12);
-	else if (c == 'x')
-		ft_putnbr_base(va_arg(arg, char*), "0123456789abcdef");
-	else if (c == 'X')
-		ft_putnbr_base(va_arg(arg, char*) "0123456789ABCDEF");
-	else if (c == 'd' || c == 'i')
-		ft_putnbr(va_arg(arg, int));
-	else if (c == 'u')
-		ft_putnbr(va_arg(arg, unsigned int));
-}
-
-static int ft_chrchr(const char *str, char c)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	ft_printf(const char *format, ...)
-{
-	va_list args;
-	char *conversion;
-	int i;
-	ft_flags flag_list;
-
-	conversion = "cspdiuxX";
-	i = 0;
-	va_start(args, format);
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			get_flags(args, format + i, flag_list, conversion);
-			i++;
-			if (ft_chrchr(conversion, format[i]))
-				put_item(format[i], args);
-			else if (format[i] == '%')
-				ft_putchar(format[i]);
-		}
-		write(1, &format[i], 1);
-		i++;
-	}
-	va_end(args);
 }
