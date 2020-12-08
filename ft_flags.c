4a26c40d5cf34c1671cdda0face30a0cb1c612f0
Dylan POINSU <dpoinsu@e2r11p5.42.fr>
2020-12-08 15:20:18 +0100

    correction norminette

--- ft_flags.c
+++ ft_flags.c
@@ -6,13 +6,13 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 11:29:12 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/12/07 11:51:22 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/12/08 15:14:50 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int ft_getdigits(const char *format)
+static int	ft_getdigits(const char *format)
 {
 	int i;
 	int res;
@@ -37,7 +37,8 @@ static void	ft_star(const char *str, ft_flags flags, int pos, va_list arg)
 		flags.width = va_arg(str, int);
 }
 
-void	get_flags(va_list args, const char *format, ft_flags flags, char *conversion)
+void		get_flags(va_list args, const char *format,
+		ft_flags flags, char *conversion)
 {
 	int i;
 
@@ -50,21 +51,13 @@ void	get_flags(va_list args, const char *format, ft_flags flags, char *conversio
 			flags.minus = 1;
 		}
 		else if (format[i] == '0' && format[i - 1] != '-')
-		{
-			i++;
-			flags.zero = ft_getdigit(format + i);
-		}
+			flags.zero = ft_getdigit(format + (i + 1));
 		else if (format[i] == '.')
-		{
-			i++;
-			flags.dot = ft_getdigit(format + i);
-		}
+			flags.dot = ft_getdigit(format + (i + 1));
 		else if (format[i] == '*')
 			ft_star(format, flags, i, args);
 		else if (isdigit)
-			flags.width = ft_getdigits(format + i);
-		else
-			i++;
-		
+			flags.width = ft_getdigits(format + (i + 1));
+		i++;
 	}
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,28 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 11:29:12 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/12/07 11:51:22 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/12/08 15:19:12 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void put_item(char c, va_list arg)
+static void put_item(char c, va_list arg, const char *str)
 {
 	if (c == 'c')
-		ft_putchar(va_arg(arg, char));
+		ft_putchar(va_arg(str, char));
 	else if (c == 's')
-		ft_putstr(va_arg(arg, char*));
+		ft_putstr(va_arg(str, char*));
 	else if (c == 'p')
 		write(1, &arg, 12);
 	else if (c == 'x')
-		ft_putnbr_base(va_arg(arg, char*), "0123456789abcdef");
+		ft_putnbr_base(va_arg(str, char*), "0123456789abcdef");
 	else if (c == 'X')
-		ft_putnbr_base(va_arg(arg, char*) "0123456789ABCDEF");
+		ft_putnbr_base(va_arg(str, char*) "0123456789ABCDEF");
 	else if (c == 'd' || c == 'i')
-		ft_putnbr(va_arg(arg, int));
+		ft_putnbr(va_arg(str, int));
 	else if (c == 'u')
-		ft_putnbr(va_arg(arg, unsigned int));
+		ft_putnbr(va_arg(str, unsigned int));
 }
 
 static int ft_chrchr(const char *str, char c)
