99140208260a0c6f4d32c42231bccdc7f245aae7
DisDylan <poinsu.dylan@gmail.com>
2020-12-09 16:57:52 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,12 +12,15 @@
 
 #include "includes/ft_printf.h"
 
-static void init_flags(ft_flags flags)
+static ft_flags init_flags(void)
 {
+	ft_flags flags;
+
 	flags.width = 0;
 	flags.minus = 0;
 	flags.zero = 0;
 	flags.dot = 0;
+	return (flags);
 }
 
 void put_item(char c, va_list arg)
@@ -49,7 +52,7 @@ int	ft_printf(const char *format, ...)
 	conversion = "cspdiuxX";
 	i = 0;
 	res = 0;
-	init_flags(flag_list);
+	flag_list = init_flags(flag_list);
 	va_start(args, format);
 	while (format[i])
 	{
