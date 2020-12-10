64df1081a909fa6c86372ef4708bfa54a9b386f0
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 11:57:48 +0100

    Update ft_fill.c

--- ft_fill.c
+++ ft_fill.c
@@ -91,10 +91,10 @@ char	*write_and_size(char *str, ft_flags flags)
 	char *tmp;
 
 	tmp = str;
-	if (flags.dot < (int)ft_strlen(str) && flags.dot > 0)
-		tmp = trunc_word(str, flags.dot);
 	if (flags.width > (int)ft_strlen(str))
 		tmp = fill_width(str, flags.width);
+	if (flags.dot < (int)ft_strlen(str) && flags.dot > 0)
+		tmp = trunc_word(str, flags.dot);
 	if (flags.zero > (int)ft_strlen(str) && flags.minus == 0)
 		tmp = fill_zero(str, flags.zero);
 	if (flags.minus == 1)
