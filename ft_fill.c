56d22dfeb639005ba53c51b391c8764058c8739c
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 15:14:07 +0100

    Update ft_fill.c

--- ft_fill.c
+++ ft_fill.c
@@ -35,7 +35,7 @@ static char *fill_width(char *tmp, int width)
 	char *str;
 	int i;
 
-	str = ft_memset((void*)str, ' ', width);
+	ft_bzero(str, width);
 	i = 0;
 	printf("Dans fill_width, str vaut %s et tmp %s\n", str, tmp);
 	newsize = width - (int)ft_strlen(tmp);
