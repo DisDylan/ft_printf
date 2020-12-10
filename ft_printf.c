fa3c422705810bf7134e974eccbae34c678e1d09
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 16:46:55 +0100

    Restart the project

--- ft_printf.c
+++ ft_printf.c
@@ -23,81 +23,93 @@ static ft_flags init_flags(void)
 	return (flags);
 }
 
-static char *put_item(char c, va_list arg)
+int ft_chrchr(char format_letter)
 {
-	char *str;
-	int nb;
-	char ch;
+	char *conversion;
+	int i;
 
-	nb = 0;
-	str = NULL;
-	ch = 0;
-	// FONCTION CHECK CHARS && CHECK INT && CHECK BASE
-	if (c == 'c')
-		ch = va_arg(arg, int);
-	if (c == 's')
-		str = va_arg(arg, char*);
-	else if (c == 'p')
-		nb = va_arg(arg, int);
-	else if (c == 'x')
-		str = ft_itoa(va_arg(arg, int));
-	//str = ft_nbr_base(va_arg(arg, int), "0123456789abcdef");
-	else if (c == 'X')
-		str = ft_itoa(va_arg(arg, int));
-	//str = ft_nbr_base(va_arg(arg, int), "0123456789ABCDEF");
-	else if (c == 'd' || c == 'i')
-		str = ft_itoa(va_arg(arg, int));
-	else if (c == 'u')
-		str = ft_itoa(va_arg(arg, unsigned int));
-	if (str != NULL)
-		return (str);
-	//if (ch != 0)
-	//	return (ch);
-	return (ft_itoa(nb));
+	conversion = "cspdiuxX";
+	i = 0;
+	while (conversion[i])
+	{
+		if (conversion[i] == format_letter)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int ft_star_or_digit(const char *format, va_list arguments, int *index)
+{
+	int count;
+	int flag_many;
+
+	count = 0;
+	if (format[0] == '*')
+	{
+		count += 1;
+		flag_many = va_arg(arg int)
+	}
+	else
+	{
+		count += ft_atoi(ft_strlen(ft_itoa(ft_atoi(format))));
+		flag_many = ft_atoi(format);
+	}
+	*index += count;
+	return (flag_many)
+}
+
+void ft_get_flag(const char *format, ft_flags *flags, va_liste arguments, int *index)
+{
+	char flag;
+
+	flag = format[0];
+	if (flag == '0' || flag == '.' || flag == '-')
+		*index += 1;
+	if (flag == '0')
+		flag->zero = ft_star_or_digit(format + 1, arguments, *index);
+	if (flag == '.')
+		flag->dot = ft_star_or_digit(format + 1, arguments, *index);
+	if (flag == '-')
+		flag->minus = 1;
+	if (ft_isdigit(flag) || flag == '*')
+		flag->width = ft_star_or_digit(format, arguments, *index);
+}
+
+ft_treat_flags(const char *format, char *printable, int *index, va_list arguments)
+{
+	ft_flags flags;
+	int new_index;
+
+	flags = init_flags();
+	new_index = 0;
+	while (!(ft_chrchr(format[new_index])))
+	{
+		ft_get_flag(format + new_index, &flags, arguments, *index);
+		new_index++;
+	}
 }
 
 int	ft_printf(const char *format, ...)
 {
-	va_list args;
-	char *conversion;
-	int i;
-	ft_flags flag_list;
-	int res;
-	char *printit;
+	va_list arguments;
+	int index;
+	char *printable;
 
-	printit = NULL;
-	conversion = "cspdiuxX";
-	i = 0;
-	res = 0;
-	va_start(args, format);
-	while (format[i])
-	{
-		if (format[i] == '%')
+	printable = NULL;	
+	va_start(arguments, format);
+	// FONCTIONS TRAITEMENT DE LA CHAINE
+	while format[index]
+		if (format[index] == '%')
 		{
-			flag_list = init_flags();
-			get_flags(args, format + i, &flag_list, conversion, &i);
-			if (ft_chrchr(conversion, format[i]))
-			{
-				printit = put_item(format[i], args);
-				printit = write_and_size(printit, &flag_list);
-				res += ft_strlen(printit);
-				ft_putstr_fd(printit, 1);
-				i++;
-			}
-			else if (format[i] == '%')
-			{
-				ft_putchar_fd(format[i], 1);
-				res++;
-			}
+			//TRAITEMENT DE LA CHAINE
+			index++;
+			ft_treat_flags(format + index, printable, &index, arguments);
+			ft_treat_convert();
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			i++;
-			res++;
-		}
+			printable[index] = format[index];
 		i++;
-	}
-	va_end(args);
-	return (res);
+	va_end(arguments);
+	return (ft_strlen(printable));
 }
