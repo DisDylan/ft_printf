6c4de69bb1deb3447db75343c1d9e03c4235bb56
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 12:06:28 +0100

    Update ft_fill.c

--- ft_fill.c
+++ ft_fill.c
@@ -99,6 +99,7 @@ char	*write_and_size(char *str, ft_flags flags)
 	char *tmp;
 
 	tmp = str;
+	printf("yo");
 	if (flags.width > (int)ft_strlen(str))
 		tmp = fill_width(str, flags.width);
 	if (flags.dot < (int)ft_strlen(str) && flags.dot > 0)
