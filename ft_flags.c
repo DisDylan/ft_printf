d49dbcb4185d5f9e3ec02e09bb30aad3b69168de
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 15:02:07 +0100

    Update ft_flags.c

--- ft_flags.c
+++ ft_flags.c
@@ -36,7 +36,7 @@ void		get_flags(va_list args, const char *format,
 {
 	int i;
 
-	i = 0;
+	i = 1;
 	printf("format dans get flags vaut : %s\n", format);
 	while (!(ft_chrchr(conversion, format[i])) && format[i])
 	{
