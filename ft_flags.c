b68bb7a1887d75172bd6c33638cb5d51c6941b6a
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 14:33:54 +0100

    Update ft_flags.c

--- ft_flags.c
+++ ft_flags.c
@@ -41,7 +41,7 @@ void		get_flags(va_list args, const char *format,
 	{
 		if (format[i] == '-')
 			flags->minus = 1;
-		else if (format[i] == '0' && flags.minus != 1)
+		else if (format[i] == '0' && flags->minus != 1)
 			flags->zero = ft_getdigits(format + (i + 1), args, &i);
 		else if (format[i] == '.')
 			flags->dot = ft_getdigits(format + (i + 1), args, &i);
