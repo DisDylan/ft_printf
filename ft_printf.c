aeeeefd5a94909da42295c156aaeedadd6a71d73
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 14:38:16 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -76,7 +76,7 @@ int	ft_printf(const char *format, ...)
 		{
 			flag_list = init_flags();
 			get_flags(args, format + i, &flag_list, conversion, &i);
-			//printf("\n\n____________________\nFLAGS VALUE:\n------%d dot %d width %d zero\n___________________\n", flag_list.dot, flag_list.width, flag_list.zero);
+			printf("\n\n____________________\nFLAGS VALUE:\n------%d dot %d width %d zero\n___________________\n", flag_list.dot, flag_list.width, flag_list.zero);
 			if (ft_chrchr(conversion, format[i]))
 			{
 				printit = put_item(format[i], args);
