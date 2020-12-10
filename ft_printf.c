fe1502d01e03eeccc5142e0cc56b719e8dac7add
DisDylan <poinsu.dylan@gmail.com>
2020-12-10 12:03:11 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -79,7 +79,7 @@ int	ft_printf(const char *format, ...)
 			if (ft_chrchr(conversion, format[i]))
 			{
 				printit = put_item(format[i], args);
-				printit = write_and_size(printit, flag_list);
+				//printit = write_and_size(printit, flag_list);
 				res += ft_strlen(printit);
 				ft_putstr_fd(printit, 1);
 				i++;
