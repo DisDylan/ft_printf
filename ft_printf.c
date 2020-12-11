18b8bb09f80cd5d16f28567b928e2516516737c8
DisDylan <poinsu.dylan@gmail.com>
2020-12-11 09:49:29 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -175,6 +175,7 @@ int	ft_printf(const char *format, ...)
 	va_start(arguments, format);
 	// FONCTIONS TRAITEMENT DE LA CHAINE
 	while (format[index])
+	{
 		if (format[index] == '%')
 		{
 			//TRAITEMENT DE LA CHAINE
@@ -192,6 +193,7 @@ int	ft_printf(const char *format, ...)
 		else
 			printable[index] = format[index];
 		index++;
+	}
 	va_end(arguments);
 	ft_putstr_fd(printable, 1);
 	return (ft_strlen(printable));
