b966c9f568b36eab999c927d0875b91f73b7f4b9
DisDylan <poinsu.dylan@gmail.com>
2020-12-11 09:47:27 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -184,7 +184,7 @@ int	ft_printf(const char *format, ...)
 				printable[index] = '%';
 				index++;
 			}
-			ft_treat_flags(format + index, &index, arguments, &flags);
+			//ft_treat_flags(format + index, &index, arguments, &flags);
 			tmp = ft_treat_all(ft_treat_convert(format + index, arguments), flags);
 			printable = ft_strjoin(printable, tmp);
 			index++;
