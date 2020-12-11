dc787d93b1b285f74a1e2df9997a3f5a640a8235
DisDylan <poinsu.dylan@gmail.com>
2020-12-11 15:23:29 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(void)
 	int pf;
 	int pft;
 
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%d$\n", 42);
 	pft = ft_printf("FT_ptf:%d$\n", 42);
 	if (pf == pft)
@@ -26,7 +26,7 @@ int main(void)
 	else
 		printf(RED "| KO |\n" RESET);
 	
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%d Hello l'ami$\n", 42);
 	pft = ft_printf("FT_ptf:%d Hello l'ami$\n", 42);
 	if (pf == pft)
@@ -42,7 +42,7 @@ int main(void)
 	else
 		printf(RED "| KO |\n" RESET);
 
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%s$\n", "une string");
 	pft = ft_printf("FT_ptf:%s$\n", "une string");
 	if (pf == pft)
@@ -67,7 +67,7 @@ int main(void)
 	else
 		printf(RED "| KO |\n" RESET);*/
 
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%10s$\n", "abcedef");
 	pft = ft_printf("FT_ptf:%10s$\n", "abcedef");
 	if (pf == pft)
@@ -75,7 +75,7 @@ int main(void)
 	else
 		printf(RED "| KO |\n" RESET);
 
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%.5s$\n", "abcdef");
 	pft = ft_printf("FT_ptf:%.5s$\n", "abcdef");
 	if (pf == pft)
@@ -83,7 +83,7 @@ int main(void)
 	else
 		printf(RED "| KO |\n" RESET);
 
-	printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
+	printf("\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%10.5s$\n", "abcedef");
 	pft = ft_printf("FT_ptf:%10.5s$\n", "abcedef");
 	if (pf == pft)
