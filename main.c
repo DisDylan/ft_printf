4d3a7267dd0977053ab7ada00bb00a39ff3951cb
DisDylan <poinsu.dylan@gmail.com>
2020-12-11 22:04:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -17,38 +17,56 @@ int main(void)
 {
 	int pf;
 	int pft;
+	int max;
+	int note;
 	
 	printf("\nTEST: d\n");
 	pf = printf("PRINTF:%d Hello l'ami$\n", 42);
 	pft = ft_printf("FT_ptf:%d Hello l'ami$\n", 42);
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	printf("\nTEST: u\n");
 	pf = printf("PRINTF:%u$\n", -42);
 	pft = ft_printf("FT_ptf:%u$\n", -42);
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	printf("\nTEST: s\n");
 	pf = printf("PRINTF:%s$\n", "une string");
 	pft = ft_printf("FT_ptf:%s$\n", "une string");
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	printf("\nTEST: %%\n");
 	pf = printf("PRINTF:%%$\n");
 	pft = ft_printf("FT_ptf:%%$\n");
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	/*printf("\n\n\n\n|||||||||||||||||||||||NEW TEST|||||||||||||||||||||\n");
 	pf = printf("PRINTF:%c\n", 'c');
@@ -63,23 +81,67 @@ int main(void)
 	pf = printf("PRINTF:%10s$\n", "abcedef");
 	pft = ft_printf("FT_ptf:%10s$\n", "abcedef");
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	printf("\nTEST: .5s\n");
 	pf = printf("PRINTF:%.5s$\n", "abcdef");
 	pft = ft_printf("FT_ptf:%.5s$\n", "abcdef");
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
 
 	printf("\nTEST: 10.5s\n");
 	pf = printf("PRINTF:%10.5s$\n", "abcedef");
 	pft = ft_printf("FT_ptf:%10.5s$\n", "abcedef");
 	if (pf == pft)
+	{
+		note++;
 		printf(GREEN "| OK |\n" RESET);
+	}
 	else
 		printf(RED "| KO |\n" RESET);
+	max++;
+
+	printf("\nTEST: 025d\n");
+	pf = printf("PRINTF:%025d$\n", 42);
+	pft = ft_printf("FT_ptf:%025d$\n", 42);
+	if (pf == pft)
+	{
+		note++;
+		printf(GREEN "| OK |\n" RESET);
+	}
+	else
+		printf(RED "| KO |\n" RESET);
+	max++;
+
+	printf("\nTEST: 10.*s\n");
+	pf = printf("PRINTF:%10.*s$\n", "abcedef", 5);
+	pft = ft_printf("FT_ptf:%10.*s$\n", "abcedef", 5);
+	if (pf == pft)
+	{
+		note++;
+		printf(GREEN "| OK |\n" RESET);
+	}
+	else
+		printf(RED "| KO |\n" RESET);
+	max++;
+
+	if (note == max)
+		printf(GREEN "BRAVO:\n%d/%d\n", note, max);
+	else
+	{
+		printf(RED "PERDU:\n%d/%d\n", note, max);
+	}
+	
 }
