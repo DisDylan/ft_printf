d366216783ceb9473fcf35c30c82d6a0d9f2eb99
DisDylan <poinsu.dylan@gmail.com>
2020-12-12 08:31:56 +0100

    Update ft_nbr_tobase.c

--- ft_nbr_tobase.c
+++ ft_nbr_tobase.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "includes/ft_printf.h"
 
 static void	is_neg(unsigned int *nb, int *nbr)
 {
