eee8547664a659442bfe8251dcfead3e15f9e7df
DisDylan <poinsu.dylan@gmail.com>
2020-12-31 11:52:01 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -111,7 +111,8 @@ static char *ft_treat_convert(const char *format, int *index, va_list arguments)
 	{
 		str = (char*)malloc(sizeof(str) * 2);
 		str[0] = va_arg(arguments, int);
-		str[1] = '\0';
+		if (str[0] != '\0')
+			str[1] = '\0';
 	}
 	if (c == '%')
 	{
