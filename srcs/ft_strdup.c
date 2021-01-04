b8f5516cd02844b4f8b034793c7cee6002b99c7c
DisDylan <poinsu.dylan@gmail.com>
2021-01-04 13:17:50 +0100

    Update ft_strdup.c

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -19,11 +19,6 @@ char	*ft_strdup(const char *s)
 
 	i = 0;
 	printf("ok la");
-	if (!s)
-	{
-		str = malloc(sizeof(*str) * 7);
-		str = "(null)";
-	}
 	while (s[i])
 		i++;
 	if (!(str = malloc(sizeof(*str) * (i + 1))))
