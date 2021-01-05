0c7f72cf8013251a103423ff6f95729fadd5fa66
Dylan POINSU <dpoinsu@e2r10p3.42.fr>
2021-01-05 09:28:21 +0100

    added all library libft

---
+++ libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 15:47:26 by dpoinsu           #+#    #+#             */
+/*   Updated: 2020/11/18 11:18:21 by dpoinsu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char*)s)[i] == (unsigned char)c)
+			return ((void*)s + i);
+		i++;
+	}
+	return (NULL);
+}
